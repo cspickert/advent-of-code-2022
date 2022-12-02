d9e7b374d02f2cf05388273f40b0a15d1287ef7c
Cameron Spickert <cameron@checkhq.com>
2022-12-02 16:47:19 -0500

    Day 2 part 1 and 2

--- day02.py
+++ day02.py
@@ -3,10 +3,42 @@ from base import BaseSolution
 
 class Solution(BaseSolution):
     def load_data(self, input):
-        pass
+        return [line.split() for line in input.splitlines()]
 
     def part1(self, data):
-        pass
+        return sum(
+            self.get_result(a, b)
+            for a, b in (self.parse_match_part1(*pair) for pair in data)
+        )
 
     def part2(self, data):
-        pass
+        return sum(
+            self.get_result(a, b)
+            for a, b in (self.parse_match_part2(*pair) for pair in data)
+        )
+
+    # Helpers
+
+    def parse_move(self, value):
+        return "ABC".index(value) + 1
+
+    def parse_match_part1(self, a, b):
+        b = b.translate(str.maketrans("XYZ", "ABC"))
+        return (self.parse_move(a), self.parse_move(b))
+
+    def parse_match_part2(self, a, b):
+        a = self.parse_move(a)
+        if b == "X":
+            b = {2: 1, 3: 2, 1: 3}[a]
+        if b == "Y":
+            b = a
+        if b == "Z":
+            b = {1: 2, 2: 3, 3: 1}[a]
+        return (a, b)
+
+    def get_result(self, a, b):
+        if a == b:
+            return b + 3
+        if (a, b) in ((1, 2), (2, 3), (3, 1)):
+            return b + 6
+        return b
