f0ea16dd44bd97c2c43bed3c871c5a8930edaa1f
Cameron Spickert <cameron@checkhq.com>
2022-12-03 13:04:29 -0500

    Day 3 part 1 and 2

--- day03.py
+++ day03.py
@@ -1,12 +1,29 @@
+import string
+
 from base import BaseSolution
 
 
 class Solution(BaseSolution):
     def load_data(self, input):
-        pass
+        return [
+            [string.ascii_letters.index(c) + 1 for c in line]
+            for line in input.splitlines()
+        ]
 
     def part1(self, data):
-        pass
+        return sum(
+            sum(a & b)
+            for a, b in [
+                (set(values[: len(values) // 2]), set(values[len(values) // 2 :]))
+                for values in data
+            ]
+        )
 
     def part2(self, data):
-        pass
+        return sum(
+            sum(a & b & c)
+            for a, b, c in [
+                [set(values) for values in data[i : i + 3]]
+                for i in range(0, len(data), 3)
+            ]
+        )
