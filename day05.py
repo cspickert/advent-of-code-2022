3b1dbb50a9b99c166e94d9d8ec455ef2710d9791
Cameron Spickert <cameron@checkhq.com>
2022-12-05 12:54:50 -0500

    Day 5 part 1

--- day05.py
+++ day05.py
@@ -1,12 +1,43 @@
+from collections import defaultdict
+
 from base import BaseSolution
 
 
 class Solution(BaseSolution):
     def load_data(self, input):
-        pass
+        diagram, instructions = input.split("\n\n")
+        return self.parse_diagram(diagram), self.parse_instructions(instructions)
 
     def part1(self, data):
-        pass
+        state, moves = data
+        for move in moves:
+            self.do_move(state, move)
+        return "".join(state[key][0] for key in sorted(state))
 
     def part2(self, data):
         pass
+
+    # Helpers
+
+    def do_move(self, state, move):
+        count, src, dst = move
+        for _ in range(int(count)):
+            state[dst].insert(0, state[src].pop(0))
+
+    def parse_diagram(self, diagram_str):
+        *stacks, keys = [
+            [line[i : i + 4][1] for i in range(0, len(line), 4)]
+            for line in diagram_str.splitlines()
+        ]
+        result = defaultdict(list)
+        for i, key in enumerate(keys):
+            for stack in stacks:
+                if i < len(stack) and stack[i] != " ":
+                    result[key].append(stack[i])
+        return result
+
+    def parse_instructions(self, instructions_str):
+        return [
+            (item[1], item[3], item[5])
+            for item in [line.split() for line in instructions_str.splitlines()]
+        ]
---
+++ tests/test_day05.py
@@ -0,0 +1,50 @@
+import pytest
+
+
+@pytest.fixture
+def example_input():
+    return """\
+    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2"""
+
+
+@pytest.fixture
+def solution():
+    from day05 import Solution
+
+    return Solution()
+
+
+@pytest.fixture
+def example_data(solution, example_input):
+    return solution.load_data(example_input)
+
+
+@pytest.fixture
+def real_data(solution):
+    from input import day05 as day05_input
+
+    return solution.load_data(day05_input)
+
+
+def test_example_part1(solution, example_data):
+    assert solution.part1(example_data) == "CMZ"
+
+
+# def test_example_part2(solution, example_data):
+#     assert solution.part2(example_data) == 4
+
+
+# def test_part1(solution, real_data):
+#     assert solution.part1(real_data) == 494
+
+
+# def test_part2(solution, real_data):
+#     assert solution.part2(real_data) == 833
