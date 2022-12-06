ff2439b9959200e61fce7750858b9ec15973cfaa
Cameron Spickert <cameron@checkhq.com>
2022-12-06 12:58:44 -0500

    Day 6 part 2

--- day06.py
+++ day06.py
@@ -2,11 +2,17 @@ from base import BaseSolution
 
 
 class Solution(BaseSolution):
-    def load_data(self, input):
-        return input
-
     def part1(self, data):
-        return next(i for i in range(4, len(data)) if len(set(data[i - 4 : i])) == 4)
+        return self.find_marker(data, 4)
 
     def part2(self, data):
-        pass
+        return self.find_marker(data, 14)
+
+    # Helper
+
+    def find_marker(self, data, num_chars):
+        return next(
+            i
+            for i in range(num_chars, len(data))
+            if len(set(data[i - num_chars : i])) == num_chars
+        )
--- tests/test_day06.py
+++ tests/test_day06.py
@@ -32,13 +32,26 @@ def test_example_part1(solution):
     assert solution.part1(data_5) == 11
 
 
-# def test_example_part2(solution, example_data):
-#     assert solution.part2(example_data) == 0
+def test_example_part2(solution):
+    data_1 = solution.load_data("mjqjpqmgbljsphdztnvjfqwrcgsmlb")
+    assert solution.part2(data_1) == 19
+
+    data_2 = solution.load_data("bvwbjplbgvbhsrlpgdmjqwftvncz")
+    assert solution.part2(data_2) == 23
+
+    data_3 = solution.load_data("nppdvjthqldpwncqszvftbrmjlhg")
+    assert solution.part2(data_3) == 23
+
+    data_4 = solution.load_data("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
+    assert solution.part2(data_4) == 29
+
+    data_5 = solution.load_data("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
+    assert solution.part2(data_5) == 26
 
 
 def test_part1(solution, real_data):
     assert solution.part1(real_data) == 1757
 
 
-# def test_part2(solution, real_data):
-#     assert solution.part2(real_data) == 0
+def test_part2(solution, real_data):
+    assert solution.part2(real_data) == 2950
