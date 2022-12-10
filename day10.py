cd0cc5910663ff868c5a7b73e4a52520c733aca5
Cameron Spickert <cameron@checkhq.com>
2022-12-10 01:02:05 -0500

    Day 10 part 1

--- day10.py
+++ day10.py
@@ -1,12 +1,59 @@
+from dataclasses import dataclass
+from itertools import chain
+
 from base import BaseSolution
+
+
+@dataclass
+class State:
+    x: int = 1
+
+
+def noop():
+    def fn(state):
+        return
+        yield
+
+    return fn
+
+
+def addx(value):
+    def fn(state):
+        yield
+        state.x += value
+
+    return fn
 
 
 class Solution(BaseSolution):
     def load_data(self, input):
-        pass
+        return [self.parse_instruction(line) for line in input.splitlines()]
 
     def part1(self, data):
-        pass
+        state = State()
+        cycle = 1
+        total_strength = 0
+        for instruction in data:
+            task = instruction(state)
+            while True:
+                try:
+                    next(task)
+                except StopIteration:
+                    break
+                finally:
+                    cycle += 1
+                    if (cycle - 20) % 40 == 0:
+                        total_strength += state.x * cycle
+        return total_strength
 
     def part2(self, data):
         pass
+
+    # Helpers
+
+    def parse_instruction(self, line):
+        line = line.split()
+        if line[0] == "noop":
+            return noop()
+        if line[0] == "addx":
+            return addx(int(line[1]))
---
+++ tests/test_day10.py
@@ -0,0 +1,187 @@
+import pytest
+
+
+@pytest.fixture
+def example_input():
+    return """\
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop"""
+
+
+@pytest.fixture
+def solution():
+    from day10 import Solution
+
+    return Solution()
+
+
+@pytest.fixture
+def example_data(solution, example_input):
+    return solution.load_data(example_input)
+
+
+@pytest.fixture
+def real_data(solution):
+    from input import day10 as day10_input
+
+    return solution.load_data(day10_input)
+
+
+def test_example_part1(solution, example_data):
+    assert solution.part1(example_data) == 13140
+
+
+# def test_example_part2(solution, example_data):
+#     assert solution.part2(example_data) == 0
+
+
+def test_part1(solution, real_data):
+    assert solution.part1(real_data) == 14220
+
+
+# def test_part2(solution, real_data):
+#     assert solution.part2(real_data) == 374400
