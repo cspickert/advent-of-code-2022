35904d1503f461d2f06786486503f1e241dd510b
Cameron Spickert <cameron@checkhq.com>
2022-12-16 22:23:30 -0500

    Day 13 part 1 example

--- day13.py
+++ day13.py
@@ -1,12 +1,40 @@
+import json
+
 from base import BaseSolution
 
 
 class Solution(BaseSolution):
     def load_data(self, input):
-        pass
+        return [
+            [json.loads(block_line) for block_line in block.splitlines()]
+            for block in input.split("\n\n")
+        ]
 
     def part1(self, data):
-        pass
+        return sum(
+            i for i, pair in enumerate(data, start=1) if self.is_pair_in_order(pair)
+        )
 
     def part2(self, data):
         pass
+
+    # Helpers
+
+    def is_pair_in_order(self, pair):
+        return self.is_pair_in_order_helper(pair) < 0
+
+    def is_pair_in_order_helper(self, pair):
+        lhs, rhs = pair
+        if isinstance(lhs, int) or isinstance(rhs, int):
+            if isinstance(lhs, list):
+                return self.is_pair_in_order_helper((lhs, [rhs]))
+            if isinstance(rhs, list):
+                return self.is_pair_in_order_helper(([lhs], rhs))
+            return lhs - rhs
+        if len(lhs) > len(rhs):
+            return 1
+        for lv, rv in zip(lhs, rhs):
+            result = self.is_pair_in_order_helper((lv, rv))
+            if result != 0:
+                return result
+        return -1
---
+++ tests/test_day13.py
@@ -0,0 +1,64 @@
+import pytest
+
+
+@pytest.fixture
+def example_input():
+    return """\
+[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]"""
+
+
+@pytest.fixture
+def solution():
+    from day13 import Solution
+
+    return Solution()
+
+
+@pytest.fixture
+def example_data(solution, example_input):
+    return solution.load_data(example_input)
+
+
+@pytest.fixture
+def real_data(solution):
+    from input import day13 as day13_input
+
+    return solution.load_data(day13_input)
+
+
+def test_example_part1(solution, example_data):
+    assert solution.part1(example_data) == 13
+
+
+# def test_example_part2(solution, example_data):
+#     assert solution.part2(example_data) == 8
+
+
+# def test_part1(solution, real_data):
+#     assert solution.part1(real_data) == 1715
+
+
+# def test_part2(solution, real_data):
+#     assert solution.part2(real_data) == 374400
